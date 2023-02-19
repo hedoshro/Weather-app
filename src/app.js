9a8c3a935d709aaa3c0ff708cc4756239e4a5473
hedie <rahimihedie22@gmail.com>
2023-02-19 10:06:30 +0330

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-prompt("How old are you?");
+function displayTemperature(response) {
+  console.log(response);
+  console.log(response.data.temperature.current);
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.temperature.current
+  );
+  document.querySelector("#city").innerHTML = response.data.city;
+  document.querySelector("#description").innerHTML =
+    response.data.condition.description;
+  document.querySelector("#humidity").innerHTML = `Humidity:
+    ${response.data.temperature.humidity} %`;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+let apiKey = "a49f0cad903e09dc8e1t8o40aab88ab3";
+let city = "istanbul";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
